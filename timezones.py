57a78f61f803041a6d8f2bc4749ed1afab178e80
Taylor Fritz <tayfritz01@gmail.com>
2020-10-28 19:57:03 -0400

    Timezone list added

---
+++ timezones.py
@@ -0,0 +1,11 @@
+import datetime
+import pytz
+
+OTHER_TIMEZONES = [
+    pytz.timezone("US/Eastern"),
+    pytz.timezone("Pacific/Auckland"),
+    pytz.timezone("Asia/Calcutta"),
+    pytz.timezone("UTC"),
+    pytz.timezone("Europe/Paris"),
+    pytz.timezone("Africa/Khartoum")
+]
