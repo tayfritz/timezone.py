4c0981361466dc159fdbd7c9e1d616e30d6b5a43
Taylor Fritz <tayfritz01@gmail.com>
2020-10-29 08:36:51 -0400

    while loop added for timezones

--- timezones.py
+++ timezones.py
@@ -1,4 +1,5 @@
 import datetime
+import _strptime
 import pytz
 
 OTHER_TIMEZONES = [
@@ -9,3 +10,24 @@ OTHER_TIMEZONES = [
     pytz.timezone("Europe/Paris"),
     pytz.timezone("Africa/Khartoum")
 ]
+
+fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+
+while True:
+    date_input = input("When is your meeting? Please use MM/DD/YYYY HH:MM format.")
+    try:
+        local_date = datetime.strptime(date_input, '%m/%d/%Y %H:%M')
+    except ValueError:
+        print("{} doesn't seem to be a valid date and time.     ".format(date_input))
+    else:
+        local_date = pytz.timezone('US/Eastern').localize(local_date)
+        utc_date = local_date.astimezone(pytz.utc)
+
+        output = []
+
+        for timezone in OTHER_TIMEZONES:
+            output.append(utc_date.astimezone(timezone))
+        for appointment in output:
+            print(appointment.strftime(appointment))
+        
+        break
